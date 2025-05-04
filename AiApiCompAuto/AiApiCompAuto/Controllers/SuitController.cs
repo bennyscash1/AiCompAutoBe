23090c91c80a7e3203ac0841608af2b776e43103
bennyscash1 <127658116+bennyscash1@users.noreply.github.com>
2025-05-04 22:38:08 +0300

    init main

---
+++ AiApiCompAuto/AiApiCompAuto/Controllers/SuitController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AiApiCompAuto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuitController : ControllerBase
+    {
+        [HttpPost("run")]
+        public IActionResult RunTest([FromBody] RunSuitCategoryName request)
+        {
+
+            #region company branch file detials
+            string solutionDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string targetPath = Path.Combine(solutionDir, "AiComprehensiveAuto", "AiCompAutoBe");
+
+            #endregion
+            #region This run the test suit by category name
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"test --filter \"Category={request.CategoryName}\"",
+                WorkingDirectory = targetPath, // שנה למיקום שלך
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            #endregion
+            var process = new Process
+            {
+                StartInfo = processInfo
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            return Ok(new
+            {
+                status = "finished",
+                exitCode = process.ExitCode,
+                output,
+                error
+            });
+        }
+
+    }
+
+    public class RunSuitCategoryName
+    {
+        public string CategoryName { get; set; }
+    }
+}
---
+++ AiApiCompAuto/AiApiCompAuto/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AiApiCompAuto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpPost("run")]
+        public IActionResult RunTestCase([FromBody] RunTestCaseName request)
+        {
+            string solutionDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string targetPath = Path.Combine(solutionDir, "AiComprehensiveAuto", "AiCompAutoBe");
+
+            #region This run the test case by name
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"test --filter \"FullyQualifiedName~{request.TestCaseName}\"",
+                WorkingDirectory = targetPath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            #endregion
+            var process = new Process
+            {
+                StartInfo = processInfo
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            return Ok(new
+            {
+                status = "finished",
+                exitCode = process.ExitCode,
+                output,
+                error
+            });
+        }
+        public class RunTestCaseName
+        {
+            public string TestCaseName { get; set; }
+        }
+    }
+}
