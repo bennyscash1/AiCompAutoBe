e4ff0c83012be4688d789d3a6f7b42b1b4a130ce
bennyscash1 <127658116+bennyscash1@users.noreply.github.com>
2025-06-25 10:23:42 +0300

    remove https

--- AiApiCompAuto/Program.cs
+++ AiApiCompAuto/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "7012";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+// בודק אם אנחנו בסביבת פיתוח או בענן
+if (builder.Environment.IsDevelopment())
+{
+    // HTTPS מקומי
+    builder.WebHost.UseUrls("https://localhost:7012");
+}
+else
+{
+    // Azure מחייב HTTP על פורט 8080
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 
 builder.Services.AddControllers();
 
